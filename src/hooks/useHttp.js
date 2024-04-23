be0c151f7258970442879cc559f901613a1abf31
simon.cheng <simon.cheng@newaim.com.au>
2024-04-23 23:16:31 +1000

    custom hook comments

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-
+// 助手函数
 async function sendHttpRequest(url, config) {
   const response = await fetch(url, config);
 
@@ -13,16 +13,18 @@ async function sendHttpRequest(url, config) {
 
   return resData;
 }
-
+// 自定义钩子函数的特点是它们可以使用React的Hooks API，比如 useState、useEffect 等。
+// 自定义钩子函数的命名通常以 "use" 开头，例如 useCustomHook。
 export default function useHttp(url, config, initialData) {
   const [data, setData] = useState(initialData);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);//在等待响应，加载状态的情况
   const [error, setError] = useState();
 
   function clearData() {
     setData(initialData);
   }
-
+// 在这个函数中，调用sendHttpRequest
+// 在UI中反应这些不同的请求状态
   const sendRequest = useCallback(
     async function sendRequest(data) {
       setIsLoading(true);
